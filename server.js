71fa4b070ac80eb42f6fb1bb6361542aeeb0eb85
root <root@31-58-245-116.localhost.local>
2026-06-10 20:49:29 +0300

    control-panel revizyon

--- server.js
+++ server.js
@@ -176,20 +176,28 @@ const server = http.createServer((req, res) => {
     let fileName;
     
     if (pathname === '/') {
-      fileName = 'index.html';
-    }
-    else if (pathname === '/urunler') {
-      fileName = 'products.html';
-    }
-    else if (pathname === '/cozumler') {
-      fileName = 'solutions.html';
-    }
-    else if (pathname === '/api/products') {
-      fileName = 'products.json';
-    }
-    else if (pathname === '/api/solutions') {
-      fileName = 'solutions.json';
-    }
+    
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(renderer.renderIndex());
+  return;
+}
+else if (pathname === '/urunler') {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(productsRenderer.renderProductList());
+  return;
+}
+else if (pathname.startsWith('/urun/')) {
+  var productId = pathname.replace('/urun/', '');
+  var html = productsRenderer.renderProductDetail(productId);
+  if (html) {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(html);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404: Ürün bulunamadı');
+  }
+  return;
+}
     else if (pathname === '/iosekrani') {
       fileName = 'ios.html';
     }   
@@ -201,17 +209,6 @@ const server = http.createServer((req, res) => {
       if (query.device) {
         console.log(`Control panel erişimi - Cihaz ID: ${query.device}`);
       }
-    }
-    else if (pathname.match(/\.(png|jpg|jpeg|webp|svg|gif)$/i)) {
-        const imgPath = path.join(__dirname, pathname);
-        const ext = path.extname(pathname).toLowerCase();
-        const mimeMap = { '.png':'image/png', '.jpg':'image/jpeg', '.jpeg':'image/jpeg', '.webp':'image/webp', '.svg':'image/svg+xml', '.gif':'image/gif' };
-        fs.readFile(imgPath, (err, content) => {
-            if (err) { res.writeHead(404); res.end('404'); return; }
-            res.writeHead(200, { 'Content-Type': mimeMap[ext] || 'image/png' });
-            res.end(content);
-        });
-        return;
     }
     else if (pathname.endsWith('.apk')) {
         const filePath = path.join(__dirname, pathname);
